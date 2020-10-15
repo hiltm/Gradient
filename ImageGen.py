9c9e132089f84d10f28a74d7ad17c158ccf6b317
hiltm <michael.hilt4@gmail.com>
2020-10-14 22:44:53 -0400

    Advancing the rectangle generation

--- ImageGen.py
+++ ImageGen.py
@@ -5,6 +5,8 @@ import random
 xaxis=900
 yaxis=800
 
+maxcolor=255
+
 #maximum number of image option
 maxoption=1
 randomnum=random.randint(0,maxoption)
@@ -16,9 +18,27 @@ if(randomnum==0):
     cv.imshow('RGB',rgb)
     #cv.waitKey(0)
 
-#ramdonizing images on black background
+#ramdonizing rectangles on black background
 elif(randomnum==1):
     img = np.zeros((xaxis,yaxis,3), np.uint8)
-    cv.rectangle(img,(384,0),(510,128),(0,255,0),10)
-    cv.imshow('shapes',img)
-    #cv.waitKey(0)
+    
+    #how many shapes to generate
+    randnumofshapes=random.randint(0,255)
+
+    for x in range(randnumofshapes):
+        ####adjustable parameters####
+        #location variables
+        xaxis1=random.randint(0,xaxis)
+        xaxis2=random.randint(0,xaxis)
+        yaxis1=random.randint(0,yaxis)
+        yaxis2=random.randint(0,yaxis)
+        #color variables
+        color1=random.randint(0,maxcolor)
+        color2=random.randint(0,maxcolor)
+        color3=random.randint(0,maxcolor)
+        #shape thickness
+        thickness=random.randint(0,10)
+    
+        cv.rectangle(img,(xaxis1,xaxis2),(yaxis1,yaxis2),(color1,color2,color3),thickness)
+        cv.imshow('shapes',img)
+        #cv.waitKey(0)
