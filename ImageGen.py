dba2d990c7d1a1469535bcebf2d24d6f741fcd92
hiltm <michael.hilt4@gmail.com>
2020-10-14 22:26:23 -0400

    Standarized image size and RNG for selecting options. This will make it easier for hourly (or timed) image generation later. Second option added for creating image on black backgrounds.

--- ImageGen.py
+++ ImageGen.py
@@ -1,6 +1,24 @@
 import numpy as np
-import cv2
+import cv2 as cv
+import random
 
-rgb = np.random.randint(255, size=(900,800,3),dtype=np.uint8)
-cv2.imshow('RGB',rgb)
-cv2.waitKey(0)
+xaxis=900
+yaxis=800
+
+#maximum number of image option
+maxoption=1
+randomnum=random.randint(0,maxoption)
+print(randomnum)
+
+#completely randomized
+if(randomnum==0):
+    rgb = np.random.randint(255, size=(xaxis,yaxis,3),dtype=np.uint8)
+    cv.imshow('RGB',rgb)
+    #cv.waitKey(0)
+
+#ramdonizing images on black background
+elif(randomnum==1):
+    img = np.zeros((xaxis,yaxis,3), np.uint8)
+    cv.rectangle(img,(384,0),(510,128),(0,255,0),10)
+    cv.imshow('shapes',img)
+    #cv.waitKey(0)
